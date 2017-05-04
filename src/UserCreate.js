cec22a295465c5fa18d08620a7174f955d2e5b9b
chasingSublimity <blakemsager@gmail.com>
2017-05-04 08:45:10 -0500

    start removing CRA boilerplate and begin bulidng user-create screen

---
+++ src/UserCreate.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './UserCreate.css';
+
+class UserCreate extends Component {
+	render() {
+		return (
+			<div className="UserCreate">
+
+				<div className="UserCreate-header">
+					<h2>Traveler New-User Screen</h2>
+				</div>
+
+				<p className="UserCreate-intro">
+				To get started, create a user below!
+				</p>
+				<form className="UserCreate-form">
+					
+				</form>
+			</div>
+		);
+	}
+}
+
+export default UserCreate;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import UserCreate from './UserCreate';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <UserCreate />,
   document.getElementById('root')
 );
