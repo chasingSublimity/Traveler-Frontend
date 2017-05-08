a78014d7c0e8b70e47e734c4936a798e43994cf9
chasingSublimity <blakemsager@gmail.com>
2017-05-08 07:59:22 -0500

    minor format changes:

--- src/components/MemoryCreateForm.js
+++ src/components/MemoryCreateForm.js
@@ -10,15 +10,20 @@ class MemoryCreateForm extends Component {
 		return (
 			<form onSubmit={handleSubmit} className="MemoryCreateForm-form" action="" method="post">
 				<label htmlFor="tripId">tripId:</label>
-				<Field component="input" className="MemoryCreateForm-input" id="tripId" type="text" name="tripId"/>			
+				<Field component="input" className="MemoryCreateForm-input" id="tripId" type="text" name="tripId"/>
+
 				<label htmlFor="image">Image:</label>
-				<Field component="input" className="MemoryCreateForm-input" id="image" type="file" name="image"/>
+				<Field component="input" className="MemoryCreateForm-input" id="image" type="file" accept="image/*" capture="camera" name="image"/>
+
 				<label htmlFor="location">Location:</label>
-				<Field component="input" className="MemoryCreateForm-input" id="location" type="text" accept="image/*" capture="camera"name="location"/>
+				<Field component="input" className="MemoryCreateForm-input" id="location" type="text" name="location"/>
+
 				<label htmlFor="date">Date:</label>
 				<Field component="input" className="MemoryCreateForm-input" id="date" type="text" name="date"/>
+
 				<label htmlFor="comments">Comments:</label>
 				<Field component="input" className="MemoryCreateForm-input" id="comments" type="text" name="comments"/>
+				
 				<input type="submit" value="Submit"/>
 			</form>
 		); 
