12d1d4c1736766d17cc609f4cb1c72585d6d3700
chasingSublimity <blakemsager@gmail.com>
2017-05-15 08:54:25 -0500

    begin working on trips display

---
+++ src/components/TripView.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions/index';
+import TripCard from './TripCard';
+// import '../css/TripCreate.css';
+
+class TripCreate extends Component {
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(values) {
+		this.props.dispatch(actions.submitTripCreateForm(values));
+	}
+
+	render() {
+		return (
+			<div className="TripCreate">
+				<p>Look at this fancy-ass form!</p>
+				<TripCreateForm onSubmit={this.handleSubmit}/>
+			</div>
+		);
+	}
+}
+
+// const mapStateToProps = (state, props) => ({
+
+// });
+
+export default connect()(TripCreate);
