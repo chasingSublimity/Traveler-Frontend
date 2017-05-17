42f208018bbe9f5a42580308d9743fc1f7b16b23
chasingSublimity <blakemsager@gmail.com>
2017-05-17 09:47:37 -0500

    add comments and reformat app component:

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Cookies from 'universal-cookie';
-
 import * as actions from '../actions/index';
 
 const cookies = new Cookies();
@@ -10,13 +9,13 @@ const cookies = new Cookies();
 class App extends Component {
 
 	componentDidMount() {
+		// if a cookie is present, save the value in the store for use communication 
+		// with the server. If the cookie is undefined, the user is redirected to the login page.
+		// Redirection is handled by router.
 		const userNameInCookie = cookies.get('userName');
-		if (!userNameInCookie) {
-			// redirect
-			console.log('redirect here');
-		} else {
-			console.log('cookie present');
+		if (userNameInCookie) {
 			this.props.dispatch(actions.setUserNameFromCookie(userNameInCookie));
+			console.log('cookie set in store');
 		}
 	}
 
