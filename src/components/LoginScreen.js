c4e3ac36d89c4041990a57a9d519deb38d75f177
chasingSublimity <blakemsager@gmail.com>
2017-05-19 09:10:17 -0500

    remove unnecessary dependency from LoginScreen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-// import * as actions from '../actions/index';
 import LoginForm from './LoginForm';
 import * as actions from '../actions/index';
 
