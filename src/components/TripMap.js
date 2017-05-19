61e5287693c1df17e4489951d8d218cf5fc7dfda
chasingSublimity <blakemsager@gmail.com>
2017-05-19 15:26:31 -0500

    begin building TripMap Component

--- src/components/TripMap.js
+++ src/components/TripMap.js
@@ -1,11 +1,54 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import Control from 'react-leaflet-control';
 
 import * as actions from '../actions/index';
 // import '../css/TripMap.css';
 
+// need to have a map container component
 class TripMap extends Component {
+	constructor(props)  {
+		super(props);
+		this.handleUpPanClick = this.handleUpPanClick.bind(this);
+		this.handleRightPanClick = this.handleRightPanClick.bind(this);
+		this.handleLeftPanClick = this.handleLeftPanClick.bind(this);
+		this.handleDownPanClick = this.handleDownPanClick.bind(this);
+	}
+
+	componentDidMount() {
+		const leafletMap = this.leafletMap.leafletElement;
+		leafletMap.on('zoomend', () => {
+			const updatedZoomLevel = leafletMap.getZoom();
+			// fire action to set zoom level in redducer
+		});
+	}
+
+	handleUpPanClick() {
+		const leafletMap = this.leafletMap.leafletElement;
+		leafletMap.panBy([0, -100]);
+		console.log('panning up');
+	}
+
+	handleRightPanClick() {
+		const leafletMap = this.leafletMap.leafletElement;
+		leafletMap.panBy([100, 0]);
+		window.console.log('Panning right');
+	}
+
+	handleLeftPanClick() {
+		const leafletMap = this.leafletMap.leafletElement;
+		leafletMap.panBy([-100, 0]);
+		window.console.log('Panning left');
+	}
+
+	handleDownPanClick() {
+		const leafletMap = this.leafletMap.leafletElement;
+		leafletMap.panBy([0, 100]);
+		window.console.log('Panning down');
+	}
+
 
 	render() {
 		return (
@@ -16,8 +59,10 @@ class TripMap extends Component {
 	}
 }
 
-// const mapStateToProps = (state, props) => ({
-
-// });
+const mapStateToProps = (state, props) => ({
+	selectedTrip: state.main.selectedTrip,
+	memories: state.main.memories,
+	selectedMemory: state.main.selectedMemory
+});
 
 export default connect(/*mapStateToProps*/)(TripMap);
