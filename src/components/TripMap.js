766bb7757fdf9db23b9548db59a40d5f6c87a853
chasingSublimity <blakemsager@gmail.com>
2017-05-21 22:53:00 -0500

    dink with map colors

--- src/components/TripMap.js
+++ src/components/TripMap.js
@@ -21,7 +21,7 @@ const mapCenter = findMidPoint(...position1, ...position2);
 
 
 
-const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
+const stamenTonerTiles = 'http://tile.stamen.com/toner/{z}/{x}/{y}.png';
 const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 // need to have a map container component
