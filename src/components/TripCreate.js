be7c6ed7593a910ab9b255b9e7c99233422eee7d
chasingSublimity <blakemsager@gmail.com>
2017-05-25 22:44:08 -0500

    wire up tripcreate to handle snackbar actions

--- src/components/TripCreate.js
+++ src/components/TripCreate.js
@@ -3,12 +3,14 @@ import {connect} from 'react-redux';
 import {reset} from 'redux-form';
 import * as actions from '../actions/index';
 import TripCreateForm from './TripCreateForm';
+import Snackbar from 'material-ui/Snackbar';
 import '../css/TripCreate.css';
 
 class TripCreate extends Component {
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleRequestClose = this.handleRequestClose.bind(this);
 	}
 
 	handleSubmit(values) {
@@ -17,18 +19,30 @@ class TripCreate extends Component {
 		this.props.dispatch(reset('tripCreate'));
 	}
 
+	handleRequestClose() {
+		this.props.dispatch(actions.closeSnackbox());
+	}
+
+
 	render() {
 		return (
 			<div className="TripCreate">
 				<p>Look at this fancy-ass form!</p>
 				<TripCreateForm onSubmit={this.handleSubmit}/>
+				<Snackbar
+					open={this.props.isSnackbarOpen}
+					message={this.props.snackbarMessage}
+					autoHideDuration={3000}
+					onRequestClose={this.handleRequestClose}
+				/>
 			</div>
 		);
 	}
 }
 
-// const mapStateToProps = (state, props) => ({
+const mapStateToProps = (state, props) => ({
+	isSnackbarOpen: state.main.isSnackbarOpen,
+	snackbarMessage: state.main.snackbarMessage
+});
 
-// });
-
-export default connect()(TripCreate);
+export default connect(mapStateToProps)(TripCreate);
