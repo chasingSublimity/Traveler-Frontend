35f774afaba6c0be0e4973b0b1f33c37b784b130
chasingSublimity <blakemsager@gmail.com>
2017-05-30 10:29:26 -0500

    begin building landing page, adjust amrgin on tripmap

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import '../css/LandingPage.css';
 
 export default class LandingPage extends Component {
 	render() {
 		return (
 			<div>
-				<p>Look at this fancy-ass landing page!</p>
+				<h1 className="landing-page-title">Traveler</h1>
+				<p>It's easy to lose track of your journeys and even easier to forget how to enjoy them.
+					<br/>
+					<span>Traveler can help.</span>
+				</p>
 			</div>
 		);
 	}
